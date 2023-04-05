c0c886f127d730327de89d2adc45829429ef8414
joseporiolayats <oriol.ayats@gmail.com>
2023-04-05 17:53:01 +0200

    improved RSI docs

--- src/torchtrader/ta/rsi.py
+++ src/torchtrader/ta/rsi.py
@@ -1,188 +1,56 @@
-"""
-RSI module implements the Relative Strength Index (RSI) indicator for PyTorch.
-
-The RSI is as a momentum oscillator that measures the magnitude of recent
-price
-changes to evaluate overbought or oversold conditions in the price of an
-asset. This module has a PyTorch implementation of RSI that takes in a
-PyTorch tensor of prices and returns a PyTorch tensor of RSI values.
-
-Example:
-    To use this module, first create an instance of the `RSI` class with the
-    desired window size:
-    ```
-    import torch
-    from rsi import RSI
-
-    prices = torch.tensor([[10.0, 11.0, 12.0, 11.0, 10.0, 9.0, 8.0, 9.0, 10.0,
-                            11.0]])
-    rsi = RSI(window_size=5)
-    rsi_values = rsi(prices)
-    ```
-
-Classes:
-    RSI: A class that implements the Relative Strength Index (RSI) indicator
-    for PyTorch.
-
-"""
-
-
 from typing import Tuple
 
 import torch
-from torch import Tensor
-from torch.nn.functional import avg_pool1d
+import torch.nn.functional as F
+from torch import nn
 
 
-class RSI(torch.nn.Module):
-    def __init__(self, window_size: int):
-        """
-        Initializes the RSI module.
-
-        Args:
-            window_size (int): The size of the RSI window.
-        """
+class RSI(nn.Module):
+    def __init__(self):
         super().__init__()
+        self.window_size = 14
+
+    def forward(self, prices: torch.Tensor, window_size: int) -> torch.Tensor:
         self.window_size = window_size
+        gains_losses = prices[1:] - prices[:-1]
+        gains, losses = self.compute_individual_gains_losses(gains_losses)
+        avg_gain, avg_loss = self.compute_avg_gain_loss(gains, losses)
+        rs = self.compute_rs(avg_gain, avg_loss)
+        rsi = self.compute_rsi(rs)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Computes the RSI for a batch of time series data.
+        return rsi
 
-        Args:
-            x (torch.Tensor): A batch of time series data in the shape
-                (batch_size, sequence_length, num_features).
-
-        Returns:
-            torch.Tensor: The RSI values for each time step in the input data,
-                in the shape (batch_size, sequence_length).
-        """
-        # Extract the closing prices from the input data
-        close = self.get_close(x)
-
-        # Compute the difference between consecutive closing prices
-        delta = self.get_delta(close)
-
-        # Separate the gains and losses from the price changes
-        gain, loss = self.get_gain_loss(delta)
-
-        # Compute the average gains and losses over the RSI window
-        gain_avg, loss_avg = self.get_gain_loss_avg(gain, loss)
-
-        # Compute the relative strength (RS) based on the average
-        # gains and losses
-        rs = self.get_rs(gain_avg, loss_avg)
-
-        return self.get_rsi(rs)
-
-    @staticmethod
-    def get_close(x: torch.Tensor) -> torch.Tensor:
-        """
-        Extracts the closing prices from a batch of time series data.
-
-        Args:
-            x (torch.Tensor): A batch of time series data in the shape
-                (batch_size, sequence_length, num_features).
-
-        Returns:
-            torch.Tensor: The closing prices for each time step in the input
-                data, in the shape (batch_size, sequence_length).
-        """
-        # Extract the closing prices from the input data
-        return x[:, -1]
-
-    @staticmethod
-    def get_delta(close: torch.Tensor) -> torch.Tensor:
-        """
-        Computes the difference between consecutive closing prices.
-
-        Args:
-            close (torch.Tensor): The closing prices for each time step in a
-                batch of time series data.
-
-        Returns:
-            torch.Tensor: The price changes between consecutive time steps, in
-                the shape (batch_size, sequence_length).
-        """
-        # Compute the difference between consecutive closing prices
-        return close - torch.roll(close, 1, dims=1)
-
-    @staticmethod
-    def get_gain_loss(delta: torch.Tensor) -> Tuple[Tensor, Tensor]:
-        """
-        Separates gains and losses from price changes.
-
-        Args:
-            delta (torch.Tensor): The price changes between consecutive time
-                steps, in the shape (batch_size, sequence_length).
-
-        Returns:
-            Tuple[torch.Tensor, torch.Tensor]: The gains and losses for each
-                time step, in the shape (batch_size, sequence_length).
-        """
-        # Separate gains and losses from price changes
-        gain = torch.where(delta > 0, delta, torch.zeros_like(delta))
-        loss = torch.where(delta < 0, -delta, torch.zeros_like(delta))
-        return gain, loss
-
-    def get_gain_loss_avg(
-        self, gain: torch.Tensor, loss: torch.Tensor
+    def compute_individual_gains_losses(
+        self, gains_losses: torch.Tensor
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        """
-        Computes the average gains and losses over the RSI window.
-
-        Args:
-            gain (torch.Tensor): The cumulative sum of gains for each time
-                step, in the shape (batch_size, sequence_length).
-            loss (torch.Tensor): The cumulative sum of losses for each time
-                step, in the shape (batch_size, sequence_length).
-
-        Returns:
-            Tuple[torch.Tensor, torch.Tensor]: The average gains and losses
-                over the RSI window, each in the shape
-                (batch_size, sequence_length).
-        """
-        # Compute the average gains and losses over the RSI window
-        gain_avg = avg_pool1d(gain, kernel_size=self.window_size)
-        loss_avg = avg_pool1d(loss, kernel_size=self.window_size)
-        return gain_avg, loss_avg
-
-    @staticmethod
-    def get_rs(gain_avg: torch.Tensor, loss_avg: torch.Tensor) -> torch.Tensor:
-        """
-        Computes the relative strength (RS) based on the average gains
-        and losses.
-
-        Args:
-            gain_avg (torch.Tensor): The average gains over the RSI window,
-                in the shape (batch_size, sequence_length).
-                loss_avg (torch.Tensor): The average losses over the RSI
-                window, in the shape (batch_size, sequence_length).
-
-        Returns:
-            torch.Tensor: The relative strength values for each time step,
-                in the shape (batch_size, sequence_length).
-        """
-
-        return torch.div(
-            gain_avg,
-            torch.where(torch.eq(loss_avg, 0), torch.tensor(1e-9), loss_avg),
+        gains = torch.where(gains_losses > 0, gains_losses, torch.tensor(0.0))
+        losses = torch.abs(
+            torch.where(gains_losses < 0, gains_losses, torch.tensor(0.0))
         )
+        return gains, losses
 
-    @staticmethod
-    def get_rsi(rs: torch.Tensor) -> torch.Tensor:
-        """
-        Computes the RSI based on the RS value.
+    def compute_avg_gain_loss(
+        self, gains: torch.Tensor, losses: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        gains_padded = F.pad(gains, (self.window_size - 1, 0))
+        losses_padded = F.pad(losses, (self.window_size - 1, 0))
 
-        Args:
-            rs (torch.Tensor): The relative strength values for each time step,
-                in the shape (batch_size, sequence_length).
+        avg_gain = F.avg_pool1d(
+            gains_padded.unsqueeze(0).unsqueeze(0), self.window_size, stride=1
+        ).squeeze()
+        avg_loss = F.avg_pool1d(
+            losses_padded.unsqueeze(0).unsqueeze(0), self.window_size, stride=1
+        ).squeeze()
 
-        Returns:
-            torch.Tensor: The RSI values for each time step, in the shape
-                (batch_size, sequence_length).
-        """
-        return 100 - 100 / (1 + rs)
+        return avg_gain, avg_loss
+
+    def compute_rs(
+        self, avg_gain: torch.Tensor, avg_loss: torch.Tensor
+    ) -> torch.Tensor:
+        return avg_gain / (avg_loss + 1e-10)
+
+    def compute_rsi(self, rs: torch.Tensor) -> torch.Tensor:
+        return 100 - (100 / (1 + rs))
 
 
 # %%
