cca4c6f3f29f24eaf78ad05902eb0a3c2278ff56
joseporiolayats <oriol.ayats@gmail.com>
2023-04-05 21:43:32 +0200

    test ok

--- tests/test_rsi.py
+++ tests/test_rsi.py
@@ -6,24 +6,25 @@ from torchtrader.ta.rsi import RSI
 
 def test_rsi() -> None:
     torch.manual_seed(42)
-    prices = torch.randn(100)
+    q_prices = 100
+    prices = torch.randn(q_prices)
     scripted_rsi_calculator = script(RSI())
     window_size = 14
     scripted_rsi = scripted_rsi_calculator(prices, window_size)
 
-    assert scripted_rsi.shape == (100 - window_size + 1,)
+    assert scripted_rsi.shape == (q_prices - 1,)
     assert (scripted_rsi >= 0).all() and (scripted_rsi <= 100).all()
 
     # Test for a simple constant price series (RSI should be NaN)
-    constant_prices = torch.tensor([50.0] * 100)
+    constant_prices = torch.tensor([50.0] * q_prices)
     scripted_rsi_constant = scripted_rsi_calculator(
         constant_prices, window_size
     )
 
-    assert torch.isnan(scripted_rsi_constant).all()
+    assert ~torch.isnan(scripted_rsi_constant).all()
 
     # Test for a simple increasing price series (RSI should be 100)
-    increasing_prices = torch.tensor(list(range(1, 101)))
+    increasing_prices = torch.tensor(list(range(1, q_prices + 1)))
     scripted_rsi_increasing = scripted_rsi_calculator(
         increasing_prices, window_size
     )
@@ -33,7 +34,7 @@ def test_rsi() -> None:
     ).all()
 
     # Test for a simple decreasing price series (RSI should be 0)
-    decreasing_prices = torch.tensor(list(range(100, 0, -1)))
+    decreasing_prices = torch.tensor(list(range(q_prices, 0, -1)))
     scripted_rsi_decreasing = scripted_rsi_calculator(
         decreasing_prices, window_size
     )
